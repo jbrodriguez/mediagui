6fb7da7cdd01d9c6f9c2713a7a2299ad4253a4ab
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-06-12 09:55:17 -0500

    - Set up skeleton for Movie item operations

--- client/js/Movie.jsx
+++ client/js/Movie.jsx
@@ -1,17 +1,43 @@
 const 	React 			= require('react'),
-		Link			= require('react-router').Link,
-		RouteHandler 	= require('react-router').RouteHandler,
-		Pager 			= require('react-paginate'),
 		DatePicker 		= require('react-datepicker'),
-		moment 			= require('moment'),
-		optionsBO 		= require('./options.js')
+		moment 			= require('moment')
 
 module.exports = React.createClass({
-	const movies = this.props.movies
-	const options = this.props.options
+	getInitialState: function() {
+		return {
+			date: moment(),
+			tmdb_id: this.props.movie.tmdb_id,
+			rating: this.props.movie.rating
+		}
+	},
 
-	var that = this;
-	var items = movies.items.map(function(movie, i) {
+	render: function() {
+		const movie = this.props.movie
+		const key = this.props.key
+		// const options = this.props.options
+
+		// var that = this;
+
+		const setStateTmdbId = function(data) {
+			return
+		}
+
+		const setStateWatched = function(data) {
+			return
+		}
+
+		const setStateRating = function(data) {
+			return
+		}
+
+		const fixTmdbId = function() {
+			return
+		}
+
+		const saveWatched = function() {
+
+		}
+		
 		var watched;
 
 		if (movie.last_watched != '') {
@@ -21,67 +47,64 @@ module.exports = React.createClass({
 		}
 
 		return (
-	
-		)
-
-	render: function() {
-		<article key={i}>
-			<div className="col-xs-12">
-				<h2>{movie.title} ({movie.year})</h2>
-			</div>
-			<div className="col-xs-12">
-				<div className="row moviep-images">
-					<div className="col-xs-12 col-sm-2">
-						<img src={"/img/p" + movie.cover} />
-					</div>
-					<div className="col-xs-12 col-sm-10">
-						<img src={"/img/b" + movie.backdrop} />
+			<article key={key}>
+				<div className="col-xs-12">
+					<h2>{movie.title} ({movie.year})</h2>
+				</div>
+				<div className="col-xs-12">
+					<div className="row moviep-images">
+						<div className="col-xs-12 col-sm-2">
+							<img src={"/img/p" + movie.cover} />
+						</div>
+						<div className="col-xs-12 col-sm-10">
+							<img src={"/img/b" + movie.backdrop} />
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="col-xs-12">
-				<div className="row between-xs">
-					<span className="col-xs-12 col-sm-6 director">{movie.director}</span>
-					<span className="col-xs-12 col-sm-6 end-sm">{movie.production_countries}</span>
-				</div>
-			</div>
-			<div className="col-xs-12">
-				<div className="row between-xs">
-					<span className="col-xs-12 col-sm-6 ">{movie.actors}</span>
-					<span className="col-xs-12 col-sm-6 end-sm">{movie.genres}</span>
-				</div>
-			</div>
-			<div className="col-xs-12 bottom-spacer">
-				<div className="row between-xs">
-					<div className="col-xs-12 col-sm-9">
-						<span className="label">{movie.resolution}</span>
-						<span className="label secondary spacer">{movie.location}</span>
-					</div>
-					<div className="col-xs-12 col-sm-3 end-sm">
-						{watched}							
-						<span className="label"><i className="icon-plus"></i>&nbsp;{moment(movie.added).format('MMM DD, YYYY H:mm')}</span>
+				<div className="col-xs-12">
+					<div className="row between-xs">
+						<span className="col-xs-12 col-sm-6 director">{movie.director}</span>
+						<span className="col-xs-12 col-sm-6 end-sm">{movie.production_countries}</span>
 					</div>
 				</div>
-			</div>
-			<div className="col-xs-12">
-				<span>{movie.overview}</span>
-			</div>
-			<div className="col-xs-12 bottom-spacer-large">
-				<div className="row between-xs">
-					<div className="col-xs-12 col-sm-10 top-xs date-picker">
-						<input type="text"></input>
-						<button className="btn btn-default">Fix</button>
-						<DatePicker
-							key="example1"
-							selected={moment()}
-							onChange={handleWatched}
-						/>
-					</div>
-					<div className="col-xs-12 col-sm-2 end-sm top-xs">
-						<span className="label"><i className="icon-watched"></i></span>
+				<div className="col-xs-12">
+					<div className="row between-xs">
+						<span className="col-xs-12 col-sm-6 ">{movie.actors}</span>
+						<span className="col-xs-12 col-sm-6 end-sm">{movie.genres}</span>
 					</div>
 				</div>
-			</div>										
-		</article>				
+				<div className="col-xs-12 bottom-spacer">
+					<div className="row between-xs">
+						<div className="col-xs-12 col-sm-9">
+							<span className="label">{movie.resolution}</span>
+							<span className="label secondary spacer">{movie.location}</span>
+						</div>
+						<div className="col-xs-12 col-sm-3 end-sm">
+							{watched}							
+							<span className="label"><i className="icon-plus"></i>&nbsp;{moment(movie.added).format('MMM DD, YYYY H:mm')}</span>
+						</div>
+					</div>
+				</div>
+				<div className="col-xs-12">
+					<span>{movie.overview}</span>
+				</div>
+				<div className="col-xs-12 bottom-spacer-large">
+					<div className="row between-xs">
+						<div className="col-xs-12 col-sm-10 top-xs date-picker">
+							<input type="text"></input>
+							<button className="btn btn-default">Fix</button>
+							<DatePicker
+								key="example1"
+								selected={this.state.date}
+								onChange={handleWatched}
+							/>
+						</div>
+						<div className="col-xs-12 col-sm-2 end-sm top-xs">
+							<span className="label"><i className="icon-watched"></i></span>
+						</div>
+					</div>
+				</div>										
+			</article>				
+		)	
 	}
 })
