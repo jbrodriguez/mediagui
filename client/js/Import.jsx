b23e064eaec075a413de9932c858b0e7386b5b95
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-06-18 07:05:50 -0500

    - Add console ui in import page

--- client/js/Import.jsx
+++ client/js/Import.jsx
@@ -8,11 +8,28 @@ module.exports = React.createClass({
 	render: function() {
 		const handleImportMovies = function() {
 			movies.importMovies()
-		}		
+		}
+
+		const items = this.props.messages.map(function(message, i) {
+			return (
+				<p key={i} className="console__line">message</p>
+			)
+		})
 	
 		return (
 			<section className="row">
-				<a href="" onClick={handleImportMovies}>import</a>
+				<div className="col-xs-12">
+					<button className="btn btn-default" onClick={handleImportMovies}>import</button>
+				</div>
+				<div className="col-xs-12">
+					<div>
+						<div className="row console" data-ng-if="vm.showConsole">
+							<div className="col-xs-12 console__lines" data-unb-scroll-bottom="vm.lines">
+								<p ng-repeat="line in vm.lines track by $index" class="console__line">{{line}}</p>
+							</div>
+						</div>						
+					</div>
+				</div>
 			</section>
 		)
 	}
