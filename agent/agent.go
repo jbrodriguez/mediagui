9049043531872737017f990a14a3d35cdee21d94
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-13 16:47:16 -0500

    (agent) Rename scanner services to  agent

---
+++ agent/agent.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	log "github.com/golang/glog"
+	"github.com/myodc/go-micro/cmd"
+	// "github.com/myodc/go-micro/examples/server/handler"
+	// "github.com/myodc/go-micro/examples/server/subscriber"
+	"github.com/myodc/go-micro/server"
+	"os"
+)
+
+func main() {
+	// optionally setup command line usage
+	cmd.Init()
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("Unable to obtain hostname: %s", err)
+	}
+
+	// Initialise Server
+	server.Init(
+		server.Name("io.jbrodriguez.mediagui.agent."+host),
+		server.Address("0.0.0.0:0"),
+	)
+
+	// Register Handlers
+	server.Handle(
+		server.NewHandler(
+			&Agent{host: host},
+		),
+	)
+
+	log.Info("Agent started ")
+
+	// Run server
+	if err := server.Run(); err != nil {
+		log.Info(err)
+	}
+
+}
