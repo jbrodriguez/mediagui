5a927bb1fdba8f8dc52dbc8ed00098d7fc809c21
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-03-09 12:50:16 -0500

    Initial vuejs code foundation
    This is a basic vue-cli, enhanced with tachyons and flexboxgrid-sass
    support.
    
    We need to add vuex now
    
    Related to #1

---
+++ client/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container body">
+    <header class="mv2 mh0">
+      <header-bar></header-bar>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer class="pv2 ph0 mv2 mh0">
+    </footer>
+  </div>
+</template>
+
+<script>
+import HeaderBar from './components/HeaderBar';
+
+export default {
+  name: 'app',
+  components: { HeaderBar },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./styles/_settings.scss";
+
+.body {
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+
+  box-sizing: border-box;
+  max-width: 78rem;
+  margin: 0 auto;
+}
+
+main {
+  flex: 1;
+}
+
+footer {
+  color: $footer-color;
+  background-color: $footer-bg;
+}
+
+.c-hlogo {
+	background-color: $headerLogoBackground;
+
+	a {
+		color: $headerLogoAnchorColor;
+	}
+}
+
+.c-hmenu {
+	background-color: $headerMenuBackground;
+}
+
+</style>
---
+++ client/src/components/HeaderBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<nav class="row between-xs">
+    <ul class="col-xs-12 col-sm-2 center-xs">
+      <li class="pv2 ph0 c-hlogo">
+        <router-link to="/">mediaGUI</router-link>
+      </li>
+    </ul>
+    <ul class="col-xs-12 col-sm-10">
+      <li class="pv2 ph0 c-hmenu">
+        <div class="row between-xs">
+          <div class="col-xs-12 col-sm-8">
+            <span class="mh3">MOVIES</span>
+          </div>
+        </div>
+      </li>
+    </ul>
+	</nav>
+</template>
+
+<script>
+export default {
+  name: 'header-bar',
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/_settings.scss";
+
+.c-hlogo {
+	background-color: $headerLogoBackground;
+
+	a {
+		color: $headerLogoAnchorColor;
+	}
+}
+
+.c-hmenu {
+	background-color: $headerMenuBackground;
+}
+</style>
