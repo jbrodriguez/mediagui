67d2de09ef287b5938bd3bcc00749f2b9958bf25
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-03-10 11:03:51 -0500

    Rename component (Cover -> Covers)
    Related to #1

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Cover from '@/components/Cover';
+import Covers from '@/components/Covers';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Cover',
-      component: Cover,
+      name: 'Covers',
+      component: Covers,
     },
   ],
 });
