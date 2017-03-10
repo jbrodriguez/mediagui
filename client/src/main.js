dffd1f403d023e24fe6da0fe37aa501cf3004ab7
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-03-10 11:04:33 -0500

    Implement single style import
    Related to #1

--- client/src/main.js
+++ client/src/main.js
@@ -3,14 +3,15 @@
 import Vue from 'vue';
 import { sync } from 'vuex-router-sync';
 
-import 'tachyons/css/tachyons.css';
-import 'flexboxgrid-sass/flexboxgrid.scss';
+// import 'tachyons/css/tachyons.css';
+// import 'flexboxgrid-sass/flexboxgrid.scss';
 
 import './styles/styles.scss';
-import App from './App';
 
 import store from './store';
 import router from './router';
+
+import App from './App';
 
 sync(store, router);
 
