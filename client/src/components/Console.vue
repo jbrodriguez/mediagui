d549c43bad02b703a0add2e7b40a6c4f9db62bc7
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-03-12 09:25:12 -0500

    Factor Console into its own component
    Related to #1

---
+++ client/src/components/Console.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="c-console">
+    <div class="row">
+      <div class="col-xs-12">
+        <p v-for="line in lines" class="console__line">{{line}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'console',
+
+  computed: {
+    lines() {
+      return this.$store.state.lines;
+    },
+  },
+
+  updated() {
+    this.$el.scrollTop = this.$el.scrollHeight;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/_settings.scss";
+
+.c-console {
+	display: flex;
+  overflow: auto;
+  // width: 100%;
+  height: 750px;
+  // height: 100%;
+  // border: 1px solid $console-border;
+  padding: 0.75em;
+  color: $console-color;
+  background-color: $console-bg;
+  // flex: 1;
+
+	.console__line {
+		font-size: 0.8em;
+		// line-height: 0.5em;
+	}
+
+  p {
+    margin-bottom: 0;
+    white-space: nowrap;
+  }
+}
+</style>
--- client/src/components/Import.vue
+++ client/src/components/Import.vue
@@ -7,13 +7,7 @@
     </section>
     <section class="row">
       <div class="col-xs-12">
-        <div class="c-console">
-          <div class="row">
-            <div class="col-xs-12">
-              <p v-for="line in lines" class="console__line">{{line}}</p>
-            </div>
-          </div>
-        </div>
+        <console></console>
       </div>
     </section>
   </div>
@@ -21,78 +15,20 @@
 
 <script>
 import * as types from '../store/types';
+import Console from './Console';
 
 export default {
   name: 'import',
 
-  // components: { Movie },
+  components: { Console },
 
   methods: {
     runImport() {
       this.$store.dispatch(types.RUN_IMPORT);
     },
   },
-
-  computed: {
-    movies() {
-      return this.$store.getters.getMovies;
-    },
-
-    lines() {
-      return this.$store.state.lines;
-    },
-  },
-
-  created() {
-    this.$store.dispatch(types.FETCH_MOVIES);
-  },
-
-  updated() {
-    this.$el.scrollTop = this.$el.scrollHeight;
-  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../styles/_settings.scss";
-
-// .fparent {
-//   position: relative
-// }
-
-// .fchild {
-//   position: absolute;
-//   height: 100%;
-// }
-
-// .flex {
-//   display: flex;
-// }
-
-// .flx_i {
-//   flex: 1;
-// }
-
-.c-console {
-	display: flex;
-  overflow: auto;
-  // width: 100%;
-  height: 750px;
-  // height: 100%;
-  // border: 1px solid $console-border;
-  padding: 0.75em;
-  color: $console-color;
-  background-color: $console-bg;
-  // flex: 1;
-
-	.console__line {
-		font-size: 0.8em;
-		// line-height: 0.5em;
-	}
-
-  p {
-    margin-bottom: 0;
-    white-space: nowrap;
-  }
-}
 </style>
