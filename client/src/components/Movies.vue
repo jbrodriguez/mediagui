c83d73e2e22b8298378d5c8b5f0144a642fb7bbe
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-03-22 12:24:03 -0500

    Switch to upstream vuejs-paginate
    It now supports forcePage, so we use it stock.
    
    Related to #21

--- client/src/components/Movies.vue
+++ client/src/components/Movies.vue
@@ -2,7 +2,7 @@
 	<section v-if="total > 0">
     <div class="row between-xs middle-xs mb2">
       <div class="col-xs-12 col-sm-10 middle-xs">
-        <pager
+        <paginate
           :pageCount="pageCount"
           :pageRange="5"
           :marginPages="3"
@@ -21,7 +21,7 @@
 
     <div class="row">
       <div class="col-xs-12 middle-xs">
-        <pager
+        <paginate
           :pageCount="pageCount"
           :pageRange="5"
           :marginPages="3"
@@ -35,16 +35,16 @@
 </template>
 
 <script>
-// import Paginate from 'vuejs-paginate';
+import Paginate from 'vuejs-paginate'
 
 import Movie from './Movie'
-import Pager from './Pager'
+// import Pager from './Pager'
 import * as types from '../store/types'
 
 export default {
   name: 'movies',
 
-  components: { Pager, Movie },
+  components: { Paginate, Movie },
 
   data () {
     return {
