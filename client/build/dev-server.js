d878f5bd8a61cbff742c2746e53869e9f2ca469d
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-09-15 10:16:03 -0500

    Update vue template
    Ref #37

--- client/build/dev-server.js
+++ client/build/dev-server.js
@@ -2,7 +2,7 @@ require('./check-versions')()
 
 var config = require('../config')
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+	process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
 var opn = require('opn')
@@ -10,9 +10,7 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
-  ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+var webpackConfig = require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
@@ -26,28 +24,29 @@ var app = express()
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
+	publicPath: webpackConfig.output.publicPath,
+	quiet: true,
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {}
+	log: false,
+	heartbeat: 2000,
 })
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
+compiler.plugin('compilation', function(compilation) {
+	compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+		hotMiddleware.publish({ action: 'reload' })
+		cb()
+	})
 })
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(options.filter || context, options))
+Object.keys(proxyTable).forEach(function(context) {
+	var options = proxyTable[context]
+	if (typeof options === 'string') {
+		options = { target: options }
+	}
+	app.use(proxyMiddleware(options.filter || context, options))
 })
 
 // handle fallback for HTML5 history API
@@ -66,18 +65,26 @@ app.use(staticPath, express.static('./static'))
 
 var uri = 'http://localhost:' + port
 
-devMiddleware.waitUntilValid(function () {
-  console.log('> Listening at ' + uri + '\n')
+var _resolve
+var readyPromise = new Promise(resolve => {
+	_resolve = resolve
 })
 
-module.exports = app.listen(port, function (err) {
-  if (err) {
-    console.log(err)
-    return
-  }
-
-  // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
+console.log('> Starting dev server...')
+devMiddleware.waitUntilValid(() => {
+	console.log('> Listening at ' + uri + '\n')
+	// when env is testing, don't need open it
+	if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
+		opn(uri)
+	}
+	_resolve()
 })
+
+var server = app.listen(port)
+
+module.exports = {
+	ready: readyPromise,
+	close: () => {
+		server.close()
+	},
+}
