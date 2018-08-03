3807a7a7c61dd20b98b83232f08e8cca25efe97c
Juan B. Rodriguez <jbrodriguez@gmail.com>
2018-08-03 10:02:23 -0500

    Fix package issues

--- server/test/main.go
+++ server/test/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"jbrodriguez/mediagui/proto"
+	"jbrodriguez/mediagui/server/src/proto"
 
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	log "github.com/golang/glog"
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
 
@@ -28,7 +28,7 @@ func main() {
 		Folders: folders,
 	})
 
-	log.Infof("req=%+v", req)
+	fmt.Printf("req=%+v\n", req)
 
 	rsp := &agent.ScanRsp{}
 
@@ -36,19 +36,19 @@ func main() {
 
 	// Call service
 	if err := client.Call(context.Background(), req, rsp); err != nil {
-		log.Warning("Unable to connect to scanning service: %s", err)
+		fmt.Printf("WARNING: Unable to connect to scanning service: %s\n", err)
 		return
 	}
 
 	𝛥t := float64(time.Since(t0)) / 1e9
 
 	for _, file := range rsp.Filenames {
-		log.Infof("file=%s", file)
+		fmt.Printf("file=%s\n", file)
 	}
 
-	log.Infof("walked %d files in %.3f seconds\n", len(rsp.Filenames), 𝛥t)
+	fmt.Printf("walked %d files in %.3f seconds\n", len(rsp.Filenames), 𝛥t)
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
-	log.Infof("Received signal %s", <-ch)
+	fmt.Printf("Received signal %s", <-ch)
 }
