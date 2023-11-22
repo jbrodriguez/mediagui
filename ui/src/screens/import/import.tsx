df0dc45f0ed569190c89393d4c9213294720fb42
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-11-22 11:53:12 -0500

    fix: button absolute position

--- ui/src/screens/import/import.tsx
+++ ui/src/screens/import/import.tsx
@@ -17,13 +17,13 @@ export const Import: React.FC = () => {
   const onClick = () => importMovies();
 
   return (
-    <div className="overflow-auto p-2 text-neutral-300 text-sm bg-slate-800 h-[64rem]">
+    <div className="relative overflow-auto p-2 text-neutral-300 text-sm bg-slate-800 h-[64rem]">
       {messages.map((line) => (
         <p className="mb0 whitespace-nowrap">{line}</p>
       ))}
       <button
         onClick={onClick}
-        className="absolute top10 right-7 bg-blue-700 text-white px-4 py-1 mb-2"
+        className="absolute right-2 bg-blue-700 text-white px-4 py-1 mb-2"
       >
         RUN
       </button>
